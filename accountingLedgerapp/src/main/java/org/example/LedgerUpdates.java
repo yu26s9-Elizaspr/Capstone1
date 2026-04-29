7e666be5d53b3f5e457ccb669d25d60d54daf8c7
elizabeth spratt <espratt@my.yearupunited.org>
2026-04-29 16:00:19 -0400

    added ledger screen method and new java file

--- accountingLedgerapp/src/main/java/org/example/LedgerUpdates.java
+++ accountingLedgerapp/src/main/java/org/example/LedgerUpdates.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 public class LedgerUpdates {
 
     // Method for "D" add deposit
-    public static void addDeposit (Scanner input) {
+    public static void addDeposit(Scanner input) {
 
         //auto-generate time/date
         LocalDate date = LocalDate.now();
@@ -42,7 +42,7 @@ public class LedgerUpdates {
     }
 
     //method for "P"
-    public static void makePayment (Scanner input) {
+    public static void makePayment(Scanner input) {
 
         // Auto generate time and date
         LocalDate date = LocalDate.now();
@@ -75,12 +75,53 @@ public class LedgerUpdates {
         }
 
 
+    }
+
+    public static void LedgerScreen(Scanner input) {
+
+        //Sout for Ledger Menu
+        System.out.println("Ledger screen: Please choose one ");
+        System.out.println(); // Space
+        System.out.println("A - View All ");
+        System.out.println("D - View Deposits");
+        System.out.println("P - View Payments ");
+        System.out.println("R - View Reports");
+        System.out.println("H - Go Back to Home Page");
 
 
+        // Prompt user for choice
+        String ledgerChoice = input.nextLine();
+
+        //switch statements ?
+        switch (ledgerChoice) {
+            case "A":
+
+                break;
+            case "D":
+                break;
+            case "P":
+                break;
+            case "R":
+                break;
+            case "H":
+                break;
+            default:
+                System.out.println("Invalid input! Please try again");
+
+
+        }
 
 
 
 
     }
 
+
+
+
+    // close bracket for LedgerUpdates
 }
+
+
+
+
